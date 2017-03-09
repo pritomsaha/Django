cf96cae222bcc389bb719007242a2d2a92a2a26a
pritomsaha <pritomsaha163@gmail.com>
2017-03-09 18:17:14 +0600

    fifth commit

--- Kirr/src/shortener/models.py
+++ Kirr/src/shortener/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+from django.conf import settings
+
 from .utils import code_generator, create_shortcode
 
-# Create your models here.
+SHORTCODE_MAX=getattr(settings, 'SHORTCODE_MAX', 15)
+
+# Create your models here.	
 
 
 class KirrURLManager(models.Manager):
@@ -25,7 +29,7 @@ class KirrURLManager(models.Manager):
 
 class KirrURL(models.Model):
 	url = models.CharField(max_length = 220)
-	shortcode = models.CharField(max_length = 15, unique = True)
+	shortcode = models.CharField(max_length = SHORTCODE_MAX, unique = True)
 	updated = models.DateTimeField(auto_now = True)
 	timestamp = models.DateTimeField(auto_now_add = True)
 	active = models.BooleanField(default=True)
--- Kirr/src/shortener/views.py
+++ Kirr/src/shortener/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import View
 
+from .models import KirrURL
 # Create your views here.
+
+def kirr_redirect_view(request, shortcode=None, *args, **kwargs):
+	obj=get_object_or_404(KirrURL, shortcode__iexact=shortcode)
+	return HttpResponseRedirect(obj.url)
+
+class KirrCBView(View):
+	def get(self, request, shortcode=None, *args, **kwargs):
+		obj=get_object_or_404(KirrURL, shortcode__iexact=shortcode)
+		return HttpResponseRedirect(obj.url)
+	
